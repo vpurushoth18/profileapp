abd9d1c0b99422ef60639819a3a3dd4340370f7a
Purushoth <vpurushoth97@gmail.com>
2025-10-21 14:52:31 -0700

    my favourite restaurant

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          {'Panda Express'}<span role="img" aria-label="Panda">🐼</span> {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
